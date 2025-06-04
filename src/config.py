9b74941caca435805743fdc389558c87f47e7a4e
theosaurus <93818867+Somekindofa@users.noreply.github.com>
2025-06-04 11:47:19 +0200

    Add configuration setup for WebDAV connection

---
+++ src/config.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+ROOT = Path(__file__).resolve().parent.parent
+load_dotenv(ROOT / ".env", override=False)
+
+WEBDAV_URL = os.getenv("WEBDAV_URL")
+WEBDAV_USERNAME = os.getenv("WEBDAV_USERNAME")
+WEBDAV_PASSWORD = os.getenv("WEBDAV_PASSWORD")
+
+DEFAULT_CHUNK = 1024*1024 # 1MB
